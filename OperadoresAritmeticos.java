67ae76e39b895a813cd4369000bc67a6f2975da7
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-18 22:08:33 -0300

    terceiro programa

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,29 @@
+public class OperadoresAritmeticos {
+	public static void main (String [ ] args) {
+		int minhaIdade = 40;
+		int suaIdade = 23;
+		
+		// Adição
+		int somaIdades = minhaIdade + suaIdade;
+		System.out.println("Soma das idades: " + somaIdades);
+		
+		// Subtração
+		int diferencaIdades = minhaIdade - suaIdade;
+		System.out.println("Diferença das idades: " + diferencaIdades);
+		
+		// Multiplicação
+		int dobroDaIdade = 2 * suaIdade;
+		System.out.println("Dobro da sua idade: " + dobroDaIdade);
+		
+		// Divisão
+		int metadeDaIdade = suaIdade / 2;
+		System.out.println("Metade da sua idade: " + metadeDaIdade);
+		
+		// Divisão
+		int restoDivisao = 7 % 2;
+		System.out.println("Resto da divisão (Módulo): " + restoDivisao);
+		
+		
+		}
+
+}

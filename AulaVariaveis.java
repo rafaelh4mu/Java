f858fdc8f12904d5a0e9c48304346e3b3c4e1c8e
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-18 21:46:21 -0300

    segundo programa

---
+++ AulaVariaveis.java
@@ -0,0 +1,12 @@
+public class AulaVariaveis {
+	public static void main (String[ ] args) {
+		int minhaIdade = 23;
+		int suaIdade = 38;
+		
+		int mediaIdades = (minhaIdade + suaIdade) / 2;
+		
+		
+		System.out.println("A média das idades é: " + mediaIdades);
+	}
+	
+}

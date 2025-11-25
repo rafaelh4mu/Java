b838e5b2a6214f94b88c6c1110782aeea2663adc
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-24 21:31:29 -0300

    quinto programa

---
+++ AbreviandoOperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+public class AbreviandoOperadoresAritmeticos {
+	public static void main (String[ ] args) {
+		int quantidadeProdutos = 10;
+		
+		// quantidadeProdutos += 3;
+		// quantidadeProdutos -= 3;
+		// quantidadeProdutos *= 2;
+		// quantidadeProdutos /= 5;
+		quantidadeProdutos %= 4;
+		System.out.println(quantidadeProdutos);
+	}
+
+}

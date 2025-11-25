6dd261f8c57fda6d2a95ec54ec0bda247c71fd58
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-24 22:12:53 -0300

    setimo programa/ boolean, byte e short

---
+++ ExemploByteShort.java
@@ -0,0 +1,15 @@
+public class ExemploByteShort {
+	
+	public static void main (String[ ] args) {
+		byte idade = 127;
+		// byte idade = 128;
+		
+		short quantidadeEstoque = 32767;
+		// short quantidadeEstoque = 32768;
+		
+		System.out.println(idade);
+		System.out.println(quantidadeEstoque);
+		
+	}
+	
+}

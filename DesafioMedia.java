e64409fa403ee9e3a6b278590180d04d2cd84ee8
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-24 21:18:22 -0300

    quarto programa

---
+++ DesafioMedia.java
@@ -0,0 +1,12 @@
+public class DesafioMedia {
+	public static void main (String[ ] args) {
+		int valor1 = 20;
+		int valor2 = 30;
+		int valor3 = 100;
+		
+		int media = (valor1 + valor2 + valor3) / 3;
+		
+		System.out.println("A média das vendas é: " + media);
+		}
+		
+	}

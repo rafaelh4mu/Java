9764eff0cb90eb615dffbeb28d5edb678f536070
rafaelh4mu <rafael.oliveira200294@gmail.com>
2025-11-24 22:55:09 -0300

    oitavo programa / Int e Long

---
+++ ExemploInt.java
@@ -0,0 +1,11 @@
+public class ExemploInt {
+	
+	public static void main (String[ ] args) {
+		int populacaoUberlandia = 699_097;
+		int populacaoUberlandia2 = 2_147_483_647; 
+		// int populacaoUberlandia2 = 2_147_483_647; // não compila		
+		
+		
+	}
+	
+}
